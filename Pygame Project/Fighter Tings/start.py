00f075195a715099db9db9491118af98cfed6c97
Cesar Jack-Vallee <93673632+cesarjackv@users.noreply.github.com>
2022-08-26 15:23:30 -0400

    Add files via upload

---
+++ Pygame Project/Fighter Tings/start.py
@@ -0,0 +1,190 @@
+import pygame
+from pygame import mixer
+from fighter import Fighter
+
+mixer.init()
+pygame.init()
+
+#create game window
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Fighter X Tings')
+
+#set framerate
+clock = pygame.time.Clock()
+FPS = 60
+
+#define colors
+RED = (255, 0, 0) 
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+#define game variables
+intro_count = 3
+last_count_update = pygame.time.get_ticks()
+score = [0, 0]  #player scores [P1, P2]
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+
+#define fighter variables
+WARRIOR_SIZE = 162
+WARRIOR_SCALE = 4
+WARRIOR_OFFSET = [72, 56]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [112, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+RONIN_SIZE = 200
+RONIN_SCALE = 3.5
+RONIN_OFFSET = [83, 65]
+RONIN_DATA = [RONIN_SIZE, RONIN_SCALE, RONIN_OFFSET]
+TENGU_SIZE = 200
+TENGU_SCALE = 3.5
+TENGU_OFFSET = [83, 70]
+TENGU_DATA = [TENGU_SIZE, TENGU_SCALE, TENGU_OFFSET]
+
+#load music and sounds
+#pygame.mixer.music.load('assets/audio/music.mp3')
+#song1 = pygame.mixer.music.load('assets/audio/FLCL1.mp3')
+#song2 = pygame.mixer.music.load('assets/audio/FLCL2.mp3')
+song3 = pygame.mixer.music.load('assets/audio/Arrancar_Duel.mp3')
+pygame.mixer.music.set_volume(0.15)
+pygame.mixer.music.play(-1, 0.0, 5000)
+sword_fx = pygame.mixer.Sound('assets/audio/sword.wav')
+sword_fx.set_volume(0.2)
+magic_fx = pygame.mixer.Sound('assets/audio/magic.wav')
+magic_fx.set_volume(0.75)
+
+#load background image
+bg_image = pygame.image.load('assets/images/background/Fighting_game_misty-background.gif').convert_alpha()
+
+#load spritesheets
+warrior_sheet = pygame.image.load('assets/images/warrior/warrior.png').convert_alpha()
+wizard_sheet = pygame.image.load('assets/images/wizard/wizard.png').convert_alpha()
+ronin_sheet = pygame.image.load('assets/images/ronin/ronin.png').convert_alpha()
+tengu_sheet = pygame.image.load('assets/images/tengu/tengu.png').convert_alpha()
+
+#load victory image
+victory_img = pygame.image.load('assets/images/icons/victory.png').convert_alpha()
+
+#define # of sprites in animation
+WARRIOR_ANIMATION_STEPS = [10, 8, 1, 7, 7, 3, 7]
+WIZARD_ANIMATION_STEPS = [8, 8, 1, 8, 8, 3, 7]
+RONIN_ANIMATION_STEPS = [8, 8, 4, 6, 6, 4, 6]
+TENGU_ANIMATION_STEPS = [4, 8, 4, 4, 4, 3, 7]
+
+#define font
+count_font = pygame.font.Font('assets/fonts/turok.ttf', 80)
+score_font = pygame.font.Font('assets/fonts/turok.ttf', 30)
+
+#draw text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
+#draw background function
+def draw_bg():
+    scaled_bg = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.blit(scaled_bg, (0,0))
+
+#draw health bars
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2, y - 2, 504, 34))
+    pygame.draw.rect(screen, RED, (x, y, 500, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 500 * ratio, 30))
+
+
+#create 2 fighter instances
+#fighter_1 = Fighter(1, 300, 380, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+#fighter_2 = Fighter(2, 900, 380, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+fighter_3 = Fighter(1, 300, 380, False, RONIN_DATA, ronin_sheet, RONIN_ANIMATION_STEPS, sword_fx)
+fighter_4 = Fighter(2, 900, 380, True, TENGU_DATA, tengu_sheet, TENGU_ANIMATION_STEPS, sword_fx)
+
+#game loop
+run = True
+while run:
+
+    #framerate
+    clock.tick(FPS)
+
+    #draw background
+    draw_bg()
+
+    #show player stats
+    draw_health_bar(fighter_3.health, 20, 20)
+    draw_health_bar(fighter_4.health, 680, 20)
+    draw_text("P1: " + str(score[0]), score_font, RED, 470, 60)
+    draw_text("P2: " + str(score[1]), score_font, RED, 680, 60)
+
+    #update countdown
+    if intro_count <= 0:
+        #move fighters
+        #fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2, round_over)
+        #fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1, round_over)
+        fighter_3.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_4, round_over)
+        fighter_4.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_3, round_over)
+    else:
+        #display count timer
+        draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
+        #update count timer
+        if (pygame.time.get_ticks() - last_count_update) >= 1000:
+            intro_count -= 1
+            last_count_update = pygame.time.get_ticks()
+
+    #update fighters
+    #fighter_1.update()
+    #fighter_2.update()
+    fighter_3.update()
+    fighter_4.update()
+
+    #draw fighters
+    #fighter_1.draw(screen)
+    #fighter_2.draw(screen)
+    fighter_3.draw(screen)
+    fighter_4.draw(screen)
+
+    #check for a death
+    if round_over == False:
+        #if fighter_1.alive == False:
+            #score[1] += 1
+            #round_over = True
+            #round_over_time = pygame.time.get_ticks()
+        #elif round_over == False:
+            #if fighter_2.alive == False:
+                #score[0] += 1
+                #round_over = True
+                #round_over_time = pygame.time.get_ticks()
+        if fighter_3.alive == False:
+            score[1] += 1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+        elif round_over == False:
+            if fighter_4.alive == False:
+                score[0] += 1
+                round_over = True
+                round_over_time = pygame.time.get_ticks()
+    else:
+        screen.blit(victory_img, (460, 150))
+        if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
+            round_over = False
+            intro_count = 3
+            #fighter_1 = Fighter(1, 300, 380, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS, sword_fx)
+            #fighter_2 = Fighter(2, 900, 380, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS, magic_fx)
+            fighter_3 = Fighter(1, 300, 380, False, RONIN_DATA, ronin_sheet, RONIN_ANIMATION_STEPS, sword_fx)
+            fighter_4 = Fighter(2, 900, 380, True, TENGU_DATA, tengu_sheet, TENGU_ANIMATION_STEPS, sword_fx)
+
+    #event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    #update display
+    pygame.display.update()
+
+#exit pygame
+pygame.quit()
+
